70beff29d5f46a334f2f6d530cdd7669d0477c06
Andrew Narine <157777708+ANADelta@users.noreply.github.com>
2025-03-16 00:22:30 +0000

    Final working version of setup.sh to ensure sl runs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# Update package list and install 'sl'
 sudo apt-get update
 sudo apt-get install -y sl
-echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
-source ~/.bashrc
+
+# Add /usr/games to PATH in .bashrc if not already present
+if ! grep -q "/usr/games" ~/.bashrc; then
+  echo 'export PATH="$PATH:/usr/games"' >> ~/.bashrc
+fi
+
+# Add /usr/games to current PATH for immediate use
+export PATH="$PATH:/usr/games"
+
+# Run 'sl' to test it works
+sl
